3b24c3701c3b2f77e57a89af6e9bc536bfcf34f8
imjayjoshi <jj623196@gmail.com>
2024-02-10 09:51:31 +0530

    nothing

--- CMS/CMS_APP/models.py
+++ CMS/CMS_APP/models.py
@@ -57,3 +57,6 @@ class Subject(models.Model):
     staff = models.ForeignKey(Staff , on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.name
